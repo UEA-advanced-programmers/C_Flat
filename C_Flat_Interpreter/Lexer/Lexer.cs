0f6b7ad50dbb46d9e63f0da3743bfc1c7a0e52d6
A-Russell23 <avrrussell@gmail.com>
2022-10-20 11:07:36 +0100

    Added Lexer and test

--- C_Flat_Interpreter/Lexer/Lexer.cs
+++ C_Flat_Interpreter/Lexer/Lexer.cs
@@ -1,11 +1,107 @@
 ﻿namespace C_Flat_Interpreter.Lexer;
+using System.Collections.Generic;
+
+/*
+Assumptions - only one line can be entered. Each character is a new token
+
+This will be changed but is assumed now in order to create a simple starting lexer
+*/
+
+/*
+ * List of things to add in future
+ * TODO - Create Token class and move over token stuff
+ * TODO - Find out if I need to count the number of tokens (maybe just for testing)
+ * TODO - Finalise enum names
+ * TODO - Use our own language
+ * TODO - Consider multi-digit numbers
+ * TODO - Other operators (divide, power, root etc)
+ */
 
 public class Lexer
 {
-    public bool Lexed;
-
-    public Lexer(bool lexed)
+    private readonly string _input;
+    private readonly List<Token> _tokens = new List<Token>(); //TODO - define max with the group
+    private int _tokenCount = 0;
+    
+    ////////// Token setup here //////////
+    public enum TokenTypes { //will be added to later, names not final
+        Add,
+        Multi,
+        LeftParam,
+        RightParam,
+        Num		
+    }
+    
+    public struct Token
     {
-        this.Lexed = lexed;
+        public TokenTypes Type;
+        public char Value;
+    }
+    
+    ////////// End of Token Setup //////////
+    
+    //constructor
+    public Lexer(string input)
+    {
+        _input = input;
+    }
+
+    //TODO - might be needed for tests, otherwise get rid of this
+    public string GetInput()
+    {
+        return _input;
+    }
+
+    public Token GetFromTokenList(int placeToSearch)
+    {
+        return _tokens[placeToSearch];
+    }
+
+    public int Tokenise() //TODO - think about if this needs to return an int
+    {
+        var newToken = new Token();
+        foreach(char c in _input)
+        {
+            switch(c)
+            {
+                case ' ' :
+                    //ignore
+                    return 0;
+                case '+' :
+                    _tokenCount++; //todo - do I need this?
+                    newToken.Type = TokenTypes.Add;
+                    newToken.Value = c;
+                    break;
+                case '*' :
+                    _tokenCount++; //todo - do I need this?
+                    newToken.Type = TokenTypes.Multi;
+                    newToken.Value = c;
+                    break;
+                case '(' :
+                    _tokenCount++; //todo - do I need this?
+                    newToken.Type = TokenTypes.LeftParam;
+                    newToken.Value = c;
+                    break;
+                case ')' :
+                    _tokenCount++; //todo - do I need this?
+                    newToken.Type = TokenTypes.RightParam;
+                    newToken.Value = c;
+                    break;
+                default :
+                    if (char.IsDigit(c))
+                    {
+                        _tokenCount++;
+                        newToken.Type = TokenTypes.Num;
+                        newToken.Value = c;
+                    }
+                    else
+                    {
+                        //TODO - need an error message here!
+                    }
+                    break;
+            }
+            _tokens.Add(newToken);
+        }
+        return 0;
     }
 }
---
+++ C_Flat_Tests/Tests_Unit/Lexer_Unit.cs
@@ -0,0 +1,44 @@
+﻿using C_Flat_Interpreter.Lexer;
+using NUnit.Framework;
+
+namespace C_Flat_Tests.Tests_Unit;
+
+public class LexerUnit
+{
+    private string _input;
+    private Lexer _lexer;
+    private string _tokens;
+    private List<Lexer.Token> _tokenList;
+        
+    [SetUp]
+    public void Setup()
+    {
+        _input = "1+2";
+        _lexer = new Lexer(_input);
+        _lexer.Tokenise();
+        _tokens = "";
+        _tokenList = new List<Lexer.Token>();
+            
+        for (int i = 0; i < _input.Length; i++)
+        {
+            var testToken = _lexer.GetFromTokenList(i);
+                
+            _tokens += testToken.Value;
+            _tokenList.Add(testToken);
+        }
+    }
+
+    [Test]
+    public void LexerUnitTestOne()
+    {
+        Assert.That(_tokens, Is.EqualTo(_input));
+    }
+        
+    [Test]
+    public void LexerUnitTestTwo()
+    {
+        Assert.That(_tokenList[0].Type, Is.EqualTo(Lexer.TokenTypes.Num));
+        Assert.That(_tokenList[1].Type, Is.EqualTo(Lexer.TokenTypes.Add));
+        Assert.That(_tokenList[2].Type, Is.EqualTo(Lexer.TokenTypes.Num));
+    }
+}
