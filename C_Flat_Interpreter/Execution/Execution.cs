93fb5e9afc10963d20c4084a9e6f51de2636a45d
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2022-10-20 15:57:50 +0100

    Added Improved execution unit tests and refactored logging within execution class

--- C_Flat_Interpreter/Execution/Execution.cs
+++ C_Flat_Interpreter/Execution/Execution.cs
@@ -1,92 +1,102 @@
-﻿namespace C_Flat_Interpreter.Execution;
+﻿using C_Flat_Interpreter.Common;
+using Microsoft.Extensions.Logging;
 
-public class Execution
+namespace C_Flat_Interpreter.Execution;
+
+public class Execution : InterpreterLogger
 {
+    public enum TokenType //will be replaced by an enumerator created by the work on lex
+    {
+        PLUS,
+        STAR,
+        L_PAR,
+        R_PAR,
+        NUMBER,
+    }
     //TODO: Remove temporary static variables when Lex class is implemented
-        public struct Token
+    public struct Token
+    {
+        public Token(TokenType type, int value = 0)
         {
-            public Token(TokenType type, int value = 0)
-            {
-                this.type = type;
-                this.value = value;
-            }
-            public TokenType type { get; set; }
-            public int value { get; set; }
+            this.type = type;
+            this.value = value;
         }
+        public TokenType type { get; set; }
+        public int value { get; set; }
+    }
 
-        public Token[] Tokens;
-        public enum TokenType //will be replaced by an enumerator created by the work on lex
-        {
-            PLUS,
-            STAR,
-            L_PAR,
-            R_PAR,
-            NUMBER,
-        }
-        
-        private readonly Stack<TokenType> _opStack = new Stack<TokenType>();
-        private readonly Stack<int> _outStack = new Stack<int>();
-        private int _op1, _op2;
-        private TokenType _opType;
-        
-        public int ShuntYard()
-        {
-            var count = 0;
-            while (count < Tokens.Length)
-            {
-                switch (Tokens[count].type)
-                {
-                    case TokenType.NUMBER:
-                        _outStack.Push(Tokens[count].value); //Put numbers straight into out stack
-                        Console.Out.WriteLine($"pushed {_outStack.Peek()} to output stack");
-                        break;
-                    case TokenType.PLUS:
-                        while (_opStack.Count > 0 && _opStack.Peek() != TokenType.L_PAR) //assert stack is not empty and top is not (
-                            Evaluate();
-                        _opStack.Push(TokenType.PLUS); //push + to op stack
-                        Console.Out.WriteLine($"Pushed {_opStack.Peek()} to operator stack");
-                        break;
-                    case TokenType.STAR:
-                        while (_opStack.Count > 0 && (_opStack.Peek() != TokenType.L_PAR && _opStack.Peek() != TokenType.PLUS)) //assert stack is not empty and top is not ( or +
-                            Evaluate();
-                        _opStack.Push(TokenType.STAR); //push * to op stack
-                        Console.Out.WriteLine($"Pushed {_opStack.Peek()} to operator stack");
-                        break;
-                    case TokenType.L_PAR:
-                        _opStack.Push(TokenType.L_PAR); //push ( to op stack
-                        Console.Out.WriteLine($"Pushed {_opStack.Peek()} to operator stack");
-                        break;
-                    case TokenType.R_PAR:
-                        while (_opStack.Count > 0 && _opStack.Peek() != TokenType.L_PAR) //assert stack is not empty and top is not (
-                            Evaluate();
-                        if (_opStack.Peek() == TokenType.L_PAR) //check if top of stack is ( and pop it
-                            _opStack.Pop();
-                        break;
-                }
-                count++;
-            }
-            while(_opStack.Count > 0) //evaluate until op stack is empty
-                Evaluate();
-            
-            return _outStack.Pop(); //return final out stack value
-        }
+    private readonly ILogger _logger;
+    public Execution()
+    {
+        _logger = GetLogger("Execution");
+    }
 
-        private void Evaluate()
+    public Token[] Tokens;
+
+    private readonly Stack<TokenType> _opStack = new Stack<TokenType>();
+    private readonly Stack<int> _outStack = new Stack<int>();
+    private int _op1, _op2;
+    private TokenType _opType;
+
+    public int ShuntYard()
+    {
+        var count = 0;
+        while (count < Tokens.Length)
         {
-            _opType = _opStack.Pop();
-            _op1 = _outStack.Pop();
-            _op2 = _outStack.Pop();
-            switch (_opType) //perform evaluation based on operator
+            switch (Tokens[count].type)
             {
-                case TokenType.PLUS: //if plus add op1 and op2
-                    Console.Out.WriteLine($"adding {_op1} and {_op2}");
-                    _outStack.Push(_op1+_op2);
+                case TokenType.NUMBER:
+                    _outStack.Push(Tokens[count].value); //Put numbers straight into out stack
+                    _logger.LogInformation("Pushed {output} to operator stack", _outStack.Peek());
                     break;
-                case TokenType.STAR: //if star multiply op1 and op2
-                    Console.Out.WriteLine($"multiplying {_op1} and {_op2}");
-                    _outStack.Push(_op1 * _op2);
+                case TokenType.PLUS:
+                    while (_opStack.Count > 0 && _opStack.Peek() != TokenType.L_PAR) //assert stack is not empty and top is not (
+                        Evaluate();
+                    _opStack.Push(TokenType.PLUS); //push + to op stack
+                    _logger.LogInformation("Pushed {operator} to operator stack", _opStack.Peek());
+                    break;
+                case TokenType.STAR:
+                    while (_opStack.Count > 0 && (_opStack.Peek() != TokenType.L_PAR && _opStack.Peek() != TokenType.PLUS)) //assert stack is not empty and top is not ( or +
+                        Evaluate();
+                    _opStack.Push(TokenType.STAR); //push * to op stack
+                    _logger.LogInformation("Pushed {operator} to operator stack", _opStack.Peek());
+                    break;
+                case TokenType.L_PAR:
+                    _opStack.Push(TokenType.L_PAR); //push ( to op stack
+                    _logger.LogInformation("Pushed {operator} to operator stack", _opStack.Peek());
+                    break;
+                case TokenType.R_PAR:
+                    while (_opStack.Count > 0 && _opStack.Peek() != TokenType.L_PAR) //assert stack is not empty and top is not (
+                        Evaluate();
+                    if (_opStack.Peek() == TokenType.L_PAR) //check if top of stack is ( and pop it
+                        _opStack.Pop();
                     break;
             }
-            Console.Out.WriteLine($"Pushed result {_outStack.Peek()} to output stack");
+            count++;
         }
+        while (_opStack.Count > 0) //evaluate until op stack is empty
+            Evaluate();
+
+
+        return _outStack.Pop(); //return final out stack value
+    }
+
+    private void Evaluate()
+    {
+        _opType = _opStack.Pop();
+        _op1 = _outStack.Pop();
+        _op2 = _outStack.Pop();
+        switch (_opType) //perform evaluation based on operator
+        {
+            case TokenType.PLUS: //if plus add op1 and op2
+                _logger.LogInformation("Adding {op1} and {op2}", _op1, _op2);
+                _outStack.Push(_op1 + _op2);
+                break;
+            case TokenType.STAR: //if star multiply op1 and op2
+                _logger.LogInformation("Multiplying {op1} and {op2}", _op1, _op2);
+                _outStack.Push(_op1 * _op2);
+                break;
+        }
+        _logger.LogInformation("Pushed result {result} to output stack \n", _outStack.Peek());
+    }
 }
--- C_Flat_Tests/Tests_Unit/ExecutionUnit.cs
+++ C_Flat_Tests/Tests_Unit/ExecutionUnit.cs
@@ -1,32 +1,128 @@
 ﻿using C_Flat_Interpreter.Execution;
+using C_Flat_Tests.Common;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 
 namespace C_Flat_Tests.Tests_Unit;
 
-public class ExecutionUnit
+public class ExecutionUnit : TestLogger
 {
-    private Execution _execution;
-    private List<Execution.Token> _tokens;
-    [SetUp]
-    public void Setup()
+    private readonly Execution _execution = new();
+    private readonly ILogger _logger;
+
+    public ExecutionUnit()
     {
-        //generate token test data
-        _tokens = new List<Execution.Token>
-        {
-            new (Execution.TokenType.NUMBER, 1),
-            new (Execution.TokenType.PLUS),
-            new (Execution.TokenType.NUMBER, 3)
-        };
+        _logger = GetLogger("Execution Unit Tests");
     }
-    
+
     [Test]
-    public void Execution_ShuntYard_Returns4()
+    public void Execution_ShuntYard_SimpleAddition_ReturnsCorrectAnswer()
     {
-        _execution = new Execution
+        _logger.LogInformation("Testing simple addition: \n");
+
+        //Arrange
+        _execution.Tokens = new[]
         {
-            Tokens = _tokens.ToArray()
+            //  1+3 = 4
+            new Execution.Token(Execution.TokenType.NUMBER, 1),
+            new Execution.Token(Execution.TokenType.PLUS),
+            new Execution.Token(Execution.TokenType.NUMBER, 3),
         };
+
+        //Act
         var result = _execution.ShuntYard();
+
+        //Assert
         Assert.That(result, Is.EqualTo(4));
+    }
+
+    [Test]
+    public void Execution_ShuntYard_Multiplication_ReturnsCorrectAnswer()
+    {
+        _logger.LogInformation("Testing simple multiplication: \n");
+
+        //Arrange
+        _execution.Tokens = new[]
+        {
+            //  9×3 = 27
+            new Execution.Token(Execution.TokenType.NUMBER, 9),
+            new Execution.Token(Execution.TokenType.STAR),
+            new Execution.Token(Execution.TokenType.NUMBER, 3),
+        };
+
+        //Act
+        var result = _execution.ShuntYard();
+
+        //Assert
+        Assert.That(result, Is.EqualTo(27));
+    }
+
+    [Test]
+    public void Execution_ShuntYard_ParenthesesExpression_ReturnsCorrectAnswer()
+    {
+        _logger.LogInformation("Testing parenthesised expression: \n");
+
+        //Arrange
+        _execution.Tokens = new[]
+        { 
+            //  (9+3) × (4+2) = 72
+            new Execution.Token(Execution.TokenType.L_PAR),
+            new Execution.Token(Execution.TokenType.NUMBER, 9),
+            new Execution.Token(Execution.TokenType.PLUS),
+            new Execution.Token(Execution.TokenType.NUMBER, 3),
+            new Execution.Token(Execution.TokenType.R_PAR),
+
+            new Execution.Token(Execution.TokenType.STAR),
+
+            new Execution.Token(Execution.TokenType.L_PAR),
+            new Execution.Token(Execution.TokenType.NUMBER, 4),
+            new Execution.Token(Execution.TokenType.PLUS),
+            new Execution.Token(Execution.TokenType.NUMBER, 2),
+            new Execution.Token(Execution.TokenType.R_PAR),
+        };
+
+        //Act
+        var result = _execution.ShuntYard();
+
+        //Assert
+        Assert.That(result, Is.EqualTo(72));
+    }
+
+    [Test]
+    public void Execution_ShuntYard_NegativeNumberExpressions_ReturnsCorrectAnswers()
+    {
+        _logger.LogInformation("Testing parenthesised expression: \n");
+
+        //Arrange
+        var negativeAddition = new Execution.Token[]
+        { 
+            //  8 + -3 = 5
+            new Execution.Token(Execution.TokenType.NUMBER, 8),
+            new Execution.Token(Execution.TokenType.PLUS),
+            new Execution.Token(Execution.TokenType.NUMBER, -3),
+        };
+
+        var negativeMultiplication = new Execution.Token[]
+        { 
+            //  -5 × 3 = -15
+            new Execution.Token(Execution.TokenType.NUMBER, -5),
+            new Execution.Token(Execution.TokenType.STAR),
+            new Execution.Token(Execution.TokenType.NUMBER, 3),
+        };
+
+        //Act
+
+        //  Perform negative addition
+        _execution.Tokens = negativeAddition;
+        var additionResult = _execution.ShuntYard();
+
+        //  Perform negative multiplication
+        _execution.Tokens = negativeMultiplication;
+        var multiplicationResult = _execution.ShuntYard();
+
+        //Assert
+        Assert.That(additionResult, Is.EqualTo(5));
+        Assert.That(multiplicationResult, Is.EqualTo(-15));
+
     }
 }
