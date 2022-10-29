1246bd1af44e6bc82a4f4bd68d64fb2c8160ceb4
A-Russell23 <avrrussell@gmail.com>
2022-10-29 14:45:29 +0100

    Refactored parser to work with wider codebase, added more functionality and unit tests

---
+++ C_Flat_Tests/Tests_Unit/ParserUnit.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using C_Flat_Interpreter.Lexer;
+using C_Flat_Interpreter.Parser;
+using NUnit.Framework;
+
+namespace C_Flat_Tests.Tests_Unit;
+
+public class ParserUnit
+{
+    [Test]
+    public void Parser_Parse_DoubleMulti_ThrowsException()
+    {
+        Lexer lexer = new Lexer("1**2");
+        Parser parser = new Parser(lexer.Tokenise());
+
+        Assert.Throws(Is.TypeOf<SyntaxErrorException>().And.Message.Contains("Number expected"),
+        delegate { parser.Parse(); });
+    }
+    
+    [Test]
+    public void Parser_Parse_NoRightParam_ThrowsException()
+    {
+        Lexer lexer = new Lexer("1*(2");
+        Parser parser = new Parser(lexer.Tokenise());
+
+        Assert.Throws(Is.TypeOf<SyntaxErrorException>().And.Message.Contains("Mismatched parentheses"),
+            delegate { parser.Parse(); });
+    }
+    
+    [Test]
+    public void Parser_Parse_NoLeftParam_ThrowsException()
+    {
+        Lexer lexer = new Lexer("1*)2");
+        Parser parser = new Parser(lexer.Tokenise());
+        
+        Assert.Throws(Is.TypeOf<SyntaxErrorException>().And.Message.Contains("Number expected"),
+            delegate { parser.Parse(); });
+    }
+}
