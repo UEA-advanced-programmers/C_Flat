f584d4d1ead360284c6852c25e9d7a9abeaafce4
A-Russell23 <avrrussell@gmail.com>
2022-12-01 18:23:22 +0000

    Removed Value from Token.cs and cleaned up

--- C_Flat_Interpreter/Common/InterpreterLogger.cs
+++ C_Flat_Interpreter/Common/InterpreterLogger.cs
@@ -12,7 +12,7 @@ public abstract class InterpreterLogger
     protected void GetLogger(string category)
     {
         _category = category;
-        _logger = new LoggerConfiguration().Destructure.ByTransforming<Token>(t => new {TokenType = t.Type, Word = t.Word, Value = t.Value}).WriteTo.Console(outputTemplate:
+        _logger = new LoggerConfiguration().Destructure.ByTransforming<Token>(t => new {TokenType = t.Type, Word = t.Word}).WriteTo.Console(outputTemplate:
             $"[{{Timestamp:HH:mm:ss}} {{Level:u3}}] {category}: {{Message:lj}}{{NewLine}}{{Exception}}").WriteTo.InMemory().CreateLogger();
         memorySink = InMemorySink.Instance;
     }
--- C_Flat_Interpreter/Common/Token.cs
+++ C_Flat_Interpreter/Common/Token.cs
@@ -5,13 +5,11 @@ namespace C_Flat_Interpreter.Common;
 public class Token
 {
     public TokenType Type;
-    public object? Value;
     public string Word;
     public int Line;
-    public Token(TokenType type = default, object? value = null, int line = -1)
+    public Token(TokenType type = default, int line = -1)
     {
         Type = type;
-        Value = value;
         Line = line;
     }
 }
