aeeb857563c289e1495d508fe89eb32a2144be9f
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-01-07 12:13:44 +0000

    fix transpiler unit test

--- C_Flat_Tests/Tests_Unit/TranspilerUnit.cs
+++ C_Flat_Tests/Tests_Unit/TranspilerUnit.cs
@@ -31,12 +31,20 @@ public class TranspilerUnit : TestLogger
         
         declaration.AddChild(new (NodeType.Terminal, new Token(TokenType.Word){Word = "var"}));
         
+        var assignment = new ParseNode(NodeType.VarAssignment);
         var identifier = new ParseNode(NodeType.VarIdentifier);
         identifier.AddChild(new(NodeType.Terminal, new Token(TokenType.Word) {Word = " test"}));
-        declaration.AddChild(identifier);
+        assignment.AddChild(identifier);
         
-        declaration.AddChild(new (NodeType.Terminal, new Token(TokenType.SemiColon){Word = ";"}));
+        assignment.AddChild(new (NodeType.Terminal, new Token(TokenType.Assignment){Word = "="}));
         
+        var value = new ParseNode(NodeType.AssignmentValue);
+        var valueString = new ParseNode(NodeType.String);
+        valueString.AddChild(new(NodeType.Terminal, new Token(TokenType.String) {Word = "\"test\""}));
+        value.AddChild(valueString);
+        assignment.AddChild(value);
+        assignment.AddChild(new (NodeType.Terminal, new Token(TokenType.SemiColon){Word = ";"}));
+        declaration.AddChild(assignment);
         statement.AddChild(declaration);
         _parseTree.Add(statement);
         
